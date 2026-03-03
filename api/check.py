c1482a5d31c9fef11fde7944ecad440b2dc5dd26
angelidk23 <angelidk569@gmail.com>
2026-03-03 01:20:32 -0600

    Update check.py

--- api/check.py
+++ api/check.py
@@ -5,8 +5,6 @@ from http.server import BaseHTTPRequestHandler
 TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
 CHAT_ID = os.environ.get("CHAT_ID")
 IMVU_USER_ID = "388423457"
-JSONBIN_API_KEY = "$2a$10$LmYGxXIvdMcCrRbvxrzjsOsEmYdCL5inoTQRwUneaW3ueMrZU36qG"
-JSONBIN_BIN_ID = "69a0b62943b1c97be9a17678"
 
 def check_online():
     try:
@@ -21,36 +19,12 @@ def send_telegram(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
     requests.post(url, data={"chat_id": CHAT_ID, "text": message})
 
-def get_state():
-    url = f"https://api.jsonbin.io/v3/b/{JSONBIN_BIN_ID}/latest"
-    r = requests.get(url, headers={"X-Master-Key": JSONBIN_API_KEY})
-    return r.json()["record"]["online"]
-
-def save_state(state):
-    url = f"https://api.jsonbin.io/v3/b/{JSONBIN_BIN_ID}"
-    requests.put(url, json={"online": state}, headers={
-        "X-Master-Key": JSONBIN_API_KEY,
-        "Content-Type": "application/json"
-    })
-
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         is_online = check_online()
-        was_online = get_state()
-
-        print(f"IMVU status: {is_online} | Estado guardado: {was_online}")
-
-        if is_online is True and not was_online:
-            print("-> Cambio: offline a online, enviando notificacion")
+        print(f"IMVU status: {is_online}")
+        if is_online is True:
             send_telegram("🟢 Active")
-            save_state(True)
-        elif is_online is False and was_online:
-            print("-> Cambio: online a offline, enviando notificacion")
-            send_telegram("🔴 Inactive")
-            save_state(False)
-        else:
-            print("-> Sin cambio de estado")
-
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b"ok")
